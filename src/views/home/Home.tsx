2e4336ac70dc540f8de3db95370ceab24cab01ab
Jesus <deonatto@gmail.com>
2022-08-11 20:03:28 +0100

    features component desgin

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -2,12 +2,16 @@ import React, { Fragment } from 'react';
 import './Home.css';
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
-
+import Features from '../../components/features/Features';
 const Home: React.FC = () => {
   return (
     <Fragment>
         <Navbar/>
         <Header/>
+        <div className='home-container'>
+          <Features/>
+          <h2 className='home-title'>Browse by property type</h2>
+        </div>
     </Fragment>
   )
 }
