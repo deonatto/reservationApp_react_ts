e96e68353fd8fad7e45bfbcd4871eb386e8ce629
Jesus <deonatto@gmail.com>
2022-08-13 23:38:26 +0100

    hotel page design

--- src/views/hotel/Hotel.tsx
+++ src/views/hotel/Hotel.tsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
+import './Hotel.css';
 
 const Hotel = () => {
+  const photos = [
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1",
+    },
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707367.jpg?k=cbacfdeb8404af56a1a94812575d96f6b80f6740fd491d02c6fc3912a16d8757&o=&hp=1",
+    },
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708745.jpg?k=1aae4678d645c63e0d90cdae8127b15f1e3232d4739bdf387a6578dc3b14bdfd&o=&hp=1",
+    },
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707776.jpg?k=054bb3e27c9e58d3bb1110349eb5e6e24dacd53fbb0316b9e2519b2bf3c520ae&o=&hp=1",
+    },
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708693.jpg?k=ea210b4fa329fe302eab55dd9818c0571afba2abd2225ca3a36457f9afa74e94&o=&hp=1",
+    },
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707389.jpg?k=52156673f9eb6d5d99d3eed9386491a0465ce6f3b995f005ac71abc192dd5827&o=&hp=1",
+    },
+  ];
   return (
-    <div>Hotel</div>
-  )
-}
+    <div>
+      <Navbar />
+      <Header type="list"/>
+      <div className="hotel-container">
+        <div className="hotel-wrapper">
+          <h1 className="hotel-title">Grand Hotel</h1>
+          <div className="hotel-address">
+            <FontAwesomeIcon icon={faLocationDot}/>
+            <p>Elton St 125 New york</p>
+          </div>
+          <p className="hotel-info">Excellent location - 500m from center</p>
+          <p className="hotel-info">Book a stay over $114 at this property and get a free airport taxi</p>
+          <div className="hotel-imgs">
+            {photos.map((photo,index)=>(
+              <div className="hotel-img-container" key={index}>
+                <img src={photo.src} className="hotel-img" alt=""/>
+              </div>
+            ))}
+          </div>
+          <div className="hotel-details">
+            <div className="hotel-details-texts">
+              <h2>Stay in the heart of City</h2>
+              <p className="hotel-desc">
+                Located a 5-minute walk from St. Florian's Gate in Krakow, Tower
+                Street Apartments has accommodations with air conditioning and
+                free WiFi. The units come with hardwood floors and feature a
+                fully equipped kitchenette with a microwave, a flat-screen TV,
+                and a private bathroom with shower and a hairdryer. A fridge is
+                also offered, as well as an electric tea pot and a coffee
+                machine. Popular points of interest near the apartment include
+                Cloth Hall, Main Market Square and Town Hall Tower. The nearest
+                airport is John Paul II International Kraków–Balice, 16.1 km
+                from Tower Street Apartments, and the property offers a paid
+                airport shuttle service.
+              </p>
+            </div>
+            <div className="hotel-price">
+              <h2>Perfect for a 9-night stay!</h2>
+              <p>
+                Located in the real heart of Krakow, this property has an
+                excellent location score of 9.8!
+              </p>
+              <h3>
+                <b>$945</b> (9 nights)
+              </h3>
+              <button>Reserve or Book Now!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer/>
+    </div>
+  );
+};
 
 export default Hotel;
