cc4815cd42ec9a9d9f2b77fafd83a5e4f1220b6d
Jesus <deonatto@gmail.com>
2022-08-14 22:08:18 +0100

    show error msg

--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -1,44 +1,51 @@
-import useFeatures from '../../hooks/useFeatures';
-import './Features.css';
-const Features = () => {
+import useFeatures from "../../hooks/useFeatures";
+import "./Features.css";
+
+const Features: React.FC = () => {
   //custom hook to get count of properties in Berlin, london and madrid
-  const {data,loading, error} = useFeatures();
+  const { data, error } = useFeatures();
 
   return (
     <div className="featured-container">
-      <div className="featured-item">
-        <img
-          src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
-          alt=""
-          className="featured-img"
-        />
-        <div className="featured-titles">
-          <h1>Berlin</h1>
-          <h2></h2>
+      {error ? (
+        <h2>{error}</h2>
+      ) : (
+        <div className="featured-wrapper">
+          <div className="featured-item">
+            <img
+              src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+              alt=""
+              className="featured-img"
+            />
+            <div className="featured-titles">
+              <h1>Berlin</h1>
+              <h2>{data[0]} properties</h2>
+            </div>
+          </div>
+          <div className="featured-item">
+            <img
+              src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+              alt=""
+              className="featured-img"
+            />
+            <div className="featured-titles">
+              <h1>Madrid</h1>
+              <h2>{data[1]} properties</h2>
+            </div>
+          </div>
+          <div className="featured-item">
+            <img
+              src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
+              alt=""
+              className="featured-img"
+            />
+            <div className="featured-titles">
+              <h1>London</h1>
+              <h2>{data[2]} properties</h2>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="featured-item">
-        <img
-          src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
-          alt=""
-          className="featured-img"
-        />
-        <div className="featured-titles">
-          <h1>Madrid</h1>
-          <h2>533 properties</h2>
-        </div>
-      </div>
-      <div className="featured-item">
-        <img
-          src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
-          alt=""
-          className="featured-img"
-        />
-        <div className="featured-titles">
-          <h1>London</h1>
-          <h2>532 properties</h2>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/hooks/useFeatures.ts
+++ src/hooks/useFeatures.ts
@@ -3,26 +3,22 @@ import axios from "axios";
 
 export default function useFeatures() {
   const [data, setData] = useState<number[]>([]);
-  const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
 
   useEffect(() => {
     const fechData = async () => {
-      setLoading(true);
       try {
         const res = await axios.get(
           "http://localhost:8800/api/hotels/countByCity?cities=berlin,madrid,london"
         );
-        console.log(res);
         setData(res.data);
       } catch (err) {
         if(axios.isAxiosError(err)){
             setError(err.message)
         }
       }
-      setLoading(false);
     };
     fechData();
   }, []);
-  return { data, loading, error };
+  return { data, error };
 }
